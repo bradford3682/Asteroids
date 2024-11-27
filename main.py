5788012d263a0992627d14bcc48e06980193bfd6
naruto4282 <bradford@3082@gmail.com>
2024-11-27 14:28:55 -0800

    C: updated main import statements

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-form module import pygame, constants
-
+from constants import *
+import pygame 
 def main():
 	print("Starting asteroids!")
-
+	print(f"Screen width: {SCREEN_WIDTH}")
+	print(f"Screen height: {SCREEN_HEIGHT}")
 if __name__ == "__main__":
 	main()
