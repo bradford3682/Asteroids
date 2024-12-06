fce183eb9c5a4bf6bdfc6ac91dff6b6e6179b6ee
naruto4282 <bradford@3082@gmail.com>
2024-12-06 12:55:54 -0800

    G: =added collisions logic

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from player import *
+from asteroidfield import *
 def main():
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
@@ -10,16 +11,25 @@ def main():
 	updatable = pygame.sprite.Group()
 	drawable  = pygame.sprite.Group()
 	dt=0
+	
+	asteroid=pygame.sprite.Group()
 	Player.containers=(updatable,drawable)
+	Asteroid.containers = (asteroid, updatable, drawable)
+	AsteroidField.containers =updatable
 	ready_player1 = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2,PLAYER_RADIUS)
+	asteroid_field=AsteroidField()
 	while True:# start of game loop
 		for event in pygame.event.get():
 			if event.type== pygame.QUIT:
 				return
-		
+	
 		pygame.Surface.fill(screen,color=(0,0,0))#draw screen
 		for object in updatable:
 			object.update(dt)
+		for object in asteroid:
+			if object.collisions(ready_player1):
+				return "Game over!"
+				break
 		for object in drawable:
 			object.draw(screen)
 		
